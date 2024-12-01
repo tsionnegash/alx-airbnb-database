8bb6a8381567374617d147ca107ab0a9322a9393
TewodrosAdimas <yosefenadagim@gmail.com>
2024-12-01 20:32:26 +0300

    task 4

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -53,3 +53,39 @@ ORDER BY
     CREATE INDEX idx_booking_id ON Booking(booking_id);
     ```
 
+
+
+-- ### 4. **EXPLAIN After Refactoring**:
+
+-- After refactoring the query and creating appropriate indexes, run `EXPLAIN` again to verify improvements:
+
+EXPLAIN SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price AS booking_total_price,
+    b.status AS booking_status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email AS user_email,
+    u.phone_number AS user_phone_number,
+    p.property_id,
+    p.name AS property_name,
+    p.location AS property_location,
+    p.pricepernight AS property_price_per_night,
+    p.description AS property_description,
+    pay.payment_id,
+    pay.amount AS payment_amount,
+    pay.payment_date AS payment_date,
+    pay.payment_method AS payment_method
+FROM 
+    Booking b
+JOIN 
+    User u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+INNER JOIN 
+    Payment pay ON b.booking_id = pay.booking_id
+ORDER BY 
+    b.booking_id;
