a0876a78bb22b2d405f2611b3b167bbfce1016e2
TewodrosAdimas <yosefenadagim@gmail.com>
2024-12-01 19:52:56 +0300

    task 0

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,6 +1,6 @@
 -- ### 1. **INNER JOIN to retrieve all bookings and the respective users who made those bookings:**
 
-select b.booking_id,
+SELECT b.booking_id,
        b.property_id,
        b.start_date,
        b.end_date,
@@ -18,7 +18,7 @@ on b.user_id = u.user_id;
 
 -- ### 2. **LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews:**
 
-select p.property_id,
+SELECT p.property_id,
        p.name,
        p.description,
        p.location,
@@ -34,7 +34,7 @@ on p.property_id = r.property_id;
 
 -- ### 3. **FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user:**
 
-select u.user_id,
+SELECT u.user_id,
        u.first_name,
        u.last_name,
        u.email,
