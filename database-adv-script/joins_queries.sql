514bcb412d9120407796b2fdf2f49d119e59e379
TewodrosAdimas <yosefenadagim@gmail.com>
2024-12-01 19:55:17 +0300

    task 0

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -18,18 +18,22 @@ on b.user_id = u.user_id;
 
 -- ### 2. **LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews:**
 
-SELECT p.property_id,
-       p.name,
-       p.description,
-       p.location,
-       p.pricepernight,
-       r.review_id,
-       r.rating,
-       r.comment,
-       r.created_at as review_created_at
-  FROM property p
-  left join review r
-on p.property_id = r.property_id;
+SELECT 
+    p.property_id,
+    p.name,
+    p.description,
+    p.location,
+    p.pricepernight,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_created_at
+FROM 
+    Property p
+LEFT JOIN 
+    Review r ON p.property_id = r.property_id
+ORDER BY 
+    r.created_at DESC; 
 
 
 -- ### 3. **FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user:**
